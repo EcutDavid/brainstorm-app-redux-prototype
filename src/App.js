934a3d2fd9ae8352565b46daf24768a98fc5c1f5
David <Ecutwxguan@163.com>
2015-09-17 21:29:48 +0800

    change demo code.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ import { connect } from 'react-redux'
 //Provide function of smart component.
 //https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
 @connect((state) => ({
-  state: state,
+  state1: state,
   stateCopy: state
 }))
 
 export default class App extends Component {
   render() {
-    const { state } = this.props
+    //it converts to var state1 = this.props.state1;
+    const { state1 } = this.props
     return (
       <div>
-        <h2>{state}</h2>
+        <h2>{state1}</h2>
         <h2>Hello world!</h2>
-        <h2>{state}</h2>
+        <h2>{this.props.stateCopy}</h2>
       </div>
     )
   }
