4a1131f454c362e83bdb9f89a91c0c2454d14f5c
David <Ecutwxguan@163.com>
2015-09-17 21:15:22 +0800

    Add comment and sample code in two.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React, { Component, findDOMNode } from 'react'
 import { store } from './index'
 import { connect } from 'react-redux'
 
+//@ this language syntax called Decorators
+//Provide function of smart component.
+//https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
 @connect((state) => ({
-  state: state
+  state: state,
+  stateCopy: state
 }))
+
 export default class App extends Component {
   render() {
     const { state } = this.props
     return (
       <div>
-        {state}
-        Hello world!
+        <h2>{state}</h2>
+        <h2>Hello world!</h2>
+        <h2>{state}</h2>
       </div>
     )
   }
 }
-
-
-let anAction =  { type: "Something happened!", payload: "I said hello"}
 
 export function exampleReducer (initialState = {}, action = {}) {
   switch (action.type) {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import App from './App'
-import { exampleReducer } from './App'
+import App, { exampleReducer } from './App'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 let store = createStore(exampleReducer)
-exports.store = store
+
 React.render(
   <Provider store={store}>
     {() => <App />}
   </Provider>,
-  document.getElementById('root'))
+  document.getElementById('root'));
+
+let anAction =  { type: "Something happened!", payload: "I said hello"}
+store.dispatch(anAction);
